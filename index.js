69760fe3d233c181272697ecdd0fe45e5449aca5
Lucas Andrade <lucascorporation@live.com>
2023-03-29 17:23:12 -0300

    Servidor com Express

--- index.js
+++ index.js
@@ -1,43 +1,73 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 
 
-let server = http.createServer((req, res) => {
-
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
-
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-
-            res.end('<h1>Olá</h1>');
-            break;
+let app = express();
 
 
+app.get('/', (req, res) => {
 
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'Lucas Andrade',
-                    email: 'contato@umeet.com.br',
-                    id: 1
-                }]
-            }));
-            break;
-
-        default:
-            break;
-    }
-
-    // res.end('OK');
-
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1>');
 
 });
 
-server.listen(3000, '127.0.0.1', () => {
+
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    res.json({
+        users: [{
+            name: 'Lucas Andrade',
+            email: 'contato@umeet.com.br',
+            id: 1
+        }]
+    });
+
+});
+
+
+
+
+// let server = http.createServer((req, res) => {
+
+//     console.log('URL:', req.url);
+//     console.log('METHOD:', req.method);
+
+//     switch (req.url) {
+//         case '/':
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'text/html');
+
+//             res.end('<h1>Olá</h1>');
+//             break;
+
+
+
+//         case '/users':
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'text/html');
+
+//             res.end(JSON.stringify({
+//                 users: [{
+//                     name: 'Lucas Andrade',
+//                     email: 'contato@umeet.com.br',
+//                     id: 1
+//                 }]
+//             }));
+//             break;
+
+//         default:
+//             break;
+//     }
+
+//     // res.end('OK');
+
+
+// });
+
+app.listen(3000, '127.0.0.1', () => {
     console.log('Servidor Rodando');
 });
