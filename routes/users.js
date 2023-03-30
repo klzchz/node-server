ad8a254c6ef1f9ab56ef2ce9319d8beddabfc472
Lucas Andrade <lucascorporation@live.com>
2023-03-30 19:23:18 -0300

    editando no nedb

--- routes/users.js
+++ routes/users.js
@@ -59,4 +59,19 @@ module.exports = (app) => {
 
     });
 
+
+    routeId.put((req, res) => {
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.send(err, req, res, 400);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+
+        });
+
+    });
+
 };
